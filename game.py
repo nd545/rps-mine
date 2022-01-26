d2cbbd28a40c04a5f02fa61a8bf6690bf20e9d48
nd545 <nd545@georgetown.edu>
2022-01-26 18:19:52 -0500

    Update game.py

--- game.py
+++ game.py
@@ -5,3 +5,38 @@
 # todo: write some Python code here to satisfy the exercise objectives
 # ... https://github.com/prof-rossetti/intro-to-python/blob/main/exercises/rock-paper-scissors/README.md
 #
+#Make a plan before we write any python code: remind us of our objectives and then figure out the action plan
+
+# ASK FOR USER INPUT
+# docs.python.org/3/library/functions.html#input
+# Use an input function to get user input
+# input() wants the textual prompt that will be shown to the user --> use quotes to pass a string data type 
+# example input("What is your favourite colour")
+# this will give a textbox for user to input their answer
+# to use the value that the user inputs, you must assign a variable to the user's input 
+# example x = input("What is your favourite colour")
+# print(x)
+# print(type(x))
+
+U = input("Please choose one of : 'rock' , 'paper' or 'scissors' : ")
+print("USER CHOSE:", U)
+
+
+
+
+
+
+
+# VALIDATIONS
+
+
+
+# COMPUTER CHOICE
+
+
+
+# DETERMINE THE WINNER
+
+
+
+# FINAL RESULTS 
