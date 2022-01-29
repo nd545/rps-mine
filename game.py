3a1997aa5fcd2d2d14391e6dd5d0a6f89577d51c
nd545 <nd545@georgetown.edu>
2022-01-29 15:55:36 -0500

    almost final version

--- game.py
+++ game.py
@@ -4,39 +4,47 @@ from telnetlib import theNULL
 from tkinter import messagebox
 from typing import List
 
+print("Welcome to Rock, Paper, Scissors Game!")
+print("------------------------------------------")
+
+# VARIABLE ASSIGNMENTS
 U = input("Please choose one of : 'rock' , 'paper' or 'scissors' : ").lower()
-print("USER CHOSE:", U)
 possible_choices =  ["rock","paper","scissors"]
+result = "outcome"
+
 # VALIDATIONS
 if U not in possible_choices:
-    print("Error: Please choose 'rock, 'paper' or 'scissors' only.")
+    print("Sorry, please choose 'rock, 'paper' or 'scissors' only.")
     exit()
 
 # COMPUTER CHOICE
 Computer_Choice = random.choice(possible_choices)
-print ( "The computer chose", Computer_Choice)
 
 # DETERMINE THE WINNER
 if U == Computer_Choice:  
-    print("It's a tie")
+    result="Oof - it's a tie!" 
 
-if U == "rock" & Computer_Choice =="paper":
-    print("Computer wins!")
+if U == "rock" and Computer_Choice =="paper":
+    result="Oh no, computer wins!"
 
-if U == "paper" & Computer_Choice=="rock":
-    print("You win!")
+if U == "paper" and Computer_Choice=="rock":
+    result="You win, yay!"
 
-if U == "rock" & Computer_Choice =="scissors":
-    print("You win!")
+if U == "rock" and Computer_Choice =="scissors":
+    result="You win, yay!"
 
-if U == "scissors" & Computer_Choice =="rock":
-    print("Computer wins!")
+if U == "scissors" and Computer_Choice =="rock":
+    result="Oh no, computer wins!"
 
-if U == "paper" & Computer_Choice=="scissors":
-    print("Computer wins!")
+if U == "paper" and Computer_Choice=="scissors":
+    result="Oh no, computer wins!"
 
-if U == "scissors" & Computer_Choice=="paper":
-    print("You win!")
-
+if U == "scissors" and Computer_Choice=="paper":
+    result="You win, yay!"
 
 # FINAL RESULTS 
+print("User chose:" , U, "\n"  "Computer Chose:", Computer_Choice)
+print("-----------------------")
+print(result)
+print("--------------------------------")
+print("Thanks for playing, come again!")
