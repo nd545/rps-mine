25d7305d86a69a7a857404c02f8797be1b5465fb
nd545 <nd545@georgetown.edu>
2022-02-04 13:49:46 -0500

    extra credit 2 done

--- game.py
+++ game.py
@@ -8,27 +8,27 @@ def determine_winner(choice_1, choice_2):
     """
  # todo: write some Python here to determine the winner
     if choice_1 == choice_2:
-        result = "Oof - it's a tie!"
+        result = None
 
     if choice_1 == "rock" and choice_2 == "paper":
-        result = "Oh no, computer wins!"
+        result = "paper"
 
     if choice_1 == "paper" and choice_2 == "rock":
-        result = "You win, yay!"
+        result = "paper"
 
     if choice_1 == "rock" and choice_2 == "scissors":
-        result = "You win, yay!"
+        result = "rock"
 
     if choice_1 == "scissors" and choice_2 == "rock":
-        result = "Oh no, computer wins!"
+        result = "rock"
 
     if choice_1 == "paper" and choice_2 == "scissors":
-        result = "Oh no, computer wins!"
+        result = "scissors"
 
     if choice_1 == "scissors" and choice_2 == "paper":
-        result = "You win, yay!"
+        result = "scissors"
 
-        return result
+    return result
 
 
 if __name__ == "__main__":
@@ -62,31 +62,31 @@ if __name__ == "__main__":
 
     # DETERMINE THE WINNER
     if U == Computer_Choice:
-        result = "Oof - it's a tie!"
+        final_message = "Oof - it's a tie!"
 
     if U == "rock" and Computer_Choice == "paper":
-        result = "Oh no, computer wins!"
+        final_message = "Oh no, computer wins!"
 
     if U == "paper" and Computer_Choice == "rock":
-        result = "You win, yay!"
+        final_message = "You win, yay!"
 
     if U == "rock" and Computer_Choice == "scissors":
-        result = "You win, yay!"
+        final_message = "You win, yay!"
 
     if U == "scissors" and Computer_Choice == "rock":
-        result = "Oh no, computer wins!"
+        final_message = "Oh no, computer wins!"
 
     if U == "paper" and Computer_Choice == "scissors":
-        result = "Oh no, computer wins!"
+        final_message = "Oh no, computer wins!"
 
     if U == "scissors" and Computer_Choice == "paper":
-        result = "You win, yay!"
+        final_message = "You win, yay!"
 
     determine_winner(U, Computer_Choice)
 
     # FINAL RESULTS
     print("User chose:", U, "\n"  "Computer Chose:", Computer_Choice)
     print("-----------------------")
-    print(result)
+    print(final_message)
     print("--------------------------------")
     print("Thanks for playing, come again!")
